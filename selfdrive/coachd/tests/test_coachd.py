ff578c469d8f17d3282e207af773f37db579a62f
Jeroen Lammersma <jeroen@lammersma.dev>
2022-08-04 15:59:32 +0200

    unit tests for CoachModule inheritance

--- selfdrive/coachd/tests/test_coachd.py
+++ selfdrive/coachd/tests/test_coachd.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import unittest
+from inspect import signature
 
 from cereal import log, messaging
 from selfdrive.coachd.coachd import COACH_MODULES, CoachD
@@ -8,6 +9,7 @@ from selfdrive.coachd.modules.base import CoachModule
 
 class TestCoachD(unittest.TestCase):
 
+  # capnp schema
   def test_DrivingCoachState_struct_defined(self) -> None:
     """Ensure DrivingCoachState struct is defined in capnp schema"""
     s = "DrivingCoachState"
@@ -20,14 +22,35 @@ class TestCoachD(unittest.TestCase):
     messages = log.Event.schema.fieldnames
     self.assertTrue(field in messages, msg="%s not in Event schema" % (field))
 
-  def test_modules_defined(self) -> None:
-    """Ensure COACH_MODULES are defined in capnp schema"""
+  def test_default_modules_defined(self) -> None:
+    """Ensure default modules are defined in capnp schema"""
     fieldnames = log.DrivingCoachState.schema.fieldnames
 
     for field in COACH_MODULES.keys():
       fail_msg = "%s not in DrivingCoachState schema" % (field)
       self.assertTrue(field in fieldnames, msg=fail_msg)
 
+  # abstract base class
+  def test_default_modules_derived_from_base(self) -> None:
+    """"Verify default modules are derived from abstract base class"""
+    for module in COACH_MODULES.values():
+      fail_msg = "%s not derived from CoachModule" % (module.__name__)
+      self.assertTrue(issubclass(module, CoachModule), msg=fail_msg)
+
+  def test_default_modules_implement_update_method(self) -> None:
+    """"Verify default modules implement update method"""
+    for module in COACH_MODULES.values():
+      fail_msg = "%s does not implement update method" % (module.__name__)
+      self.assertTrue(callable(getattr(module, "update", None)), msg=fail_msg)
+
+  def test_default_modules_update_method_parameters_same_as_base(self) -> None:
+    """Verify update method parameters of default modules are same as base"""
+    sig = signature(CoachModule.update).parameters
+    for module in COACH_MODULES.values():
+      fail_msg = "%s signature differs from CoachModule" % (module.__name__)
+      self.assertEqual(sig, signature(module.update).parameters, msg=fail_msg)
+
+  # constructor
   def test_init_with_custom_module(self) -> None:
     """Verify module passed in constructor is added to coachd modules"""
     class CustomModule(CoachModule):
