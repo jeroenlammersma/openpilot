bb1e9558dac372d56435628b506960d0156d42a7
Jeroen Lammersma <jeroen@lammersma.dev>
2022-08-21 15:27:40 +0200

    change cereal from 'master' to 'driving-coach'

--- release/check-submodules.sh
+++ release/check-submodules.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 while read hash submodule ref; do
-  git -C $submodule fetch --depth 100 origin master
-  git -C $submodule branch -r --contains $hash | grep "origin/master"
+  branch=$([ "$submodule" = "cereal" ] && echo "driving-coach" || echo "master")
+  git -C $submodule fetch --depth 100 origin $branch
+  git -C $submodule branch -r --contains $hash | grep "origin/$branch"
   if [ "$?" -eq 0 ]; then
     echo "$submodule ok"
   else
-    echo "$submodule: $hash is not on master"
+    echo "$submodule: $hash is not on $branch"
     exit 1
   fi
 done <<< $(git submodule status --recursive)
